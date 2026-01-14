14a0f8d17756d69ee38d73908db297fa70e99422
Matt Derocher <matt@derocher.me>
2026-01-14 05:38:30 -0500

    added basics to messages list

---
+++ src/lib/components/MessagesList.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { faker } from '@faker-js/faker'
+
+	const friends = Array.from({ length: 20 }, () => faker.lorem.sentence())
+</script>
+
+
+<ul>
+    {#each friends as friend}
+        <li>{friend}</li>
+    {/each}
+</ul>
+
+
+<style>
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		display: grid;
+		gap: 1rem;
+		padding: 1rem;
+	}
+
+	li {
+		padding: 1rem;
+		max-width: 40ch;
+		background-color: black;
+		color: white;
+		border-radius: 10px;
+	}
+
+	li:nth-child(odd) {
+		justify-self: end;
+	}
+</style>
--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-	import { FriendsList } from '$lib/components/FriendsList.svelte';
+	import FriendsList from '$lib/components/FriendsList.svelte';
+	import MessagesList from '$lib/components/MessagesList.svelte';
 </script>
 
+<div>
 <FriendsList />
+<MessagesList />
+</div>
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: 1fr 6fr;
+	}
+</style>
