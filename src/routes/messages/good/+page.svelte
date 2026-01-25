8e9b5a6bcbf017e9f6881904adab97e9fd0e0f8f
Matt Derocher <matt@derocher.me>
2026-01-25 07:56:21 -0500

    Go to first message in list on navigation

--- src/routes/messages/good/+page.svelte
+++ src/routes/messages/good/+page.svelte
@@ -1,3 +1,33 @@
+<script lang="ts">
+	import { goto } from '$app/navigation'
+	import { friends } from '$lib/stores/friendsStore'
+	import { get } from 'svelte/store'
+	import { browser } from '$app/environment'
+
+	if (browser) {
+		// Redirect to first friend's page when landing on this route
+		$effect(() => {
+			// Get current value immediately
+			const friendsList = get(friends)
+			if (friendsList.length > 0) {
+				const firstFriend = friendsList[0]
+				goto(`/messages/good/${firstFriend.id}`, { replaceState: true })
+				return
+			}
+
+			// If no friends yet, subscribe and wait
+			const unsubscribe = friends.subscribe((friendsList) => {
+				if (friendsList.length > 0) {
+					const firstFriend = friendsList[0]
+					goto(`/messages/good/${firstFriend.id}`, { replaceState: true })
+					unsubscribe()
+				}
+			})
+			return unsubscribe
+		})
+	}
+</script>
+
 <p>Select a friend from the list to view their messages.</p>
 
 <style>
