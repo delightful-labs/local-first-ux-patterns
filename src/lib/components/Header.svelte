ace2c8389c03cd2929cda875d15bb5d41180f40a
Matt Derocher <matt@derocher.me>
2026-01-25 09:49:38 -0500

    Added logos

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,6 +6,9 @@
 <header>
 	<NetworkStatusIndicator />
 	<NetworkStatusButton />
+	<div class="logo-container">
+		<img src="/images/delightfullabs-logo.svg" alt="Delightful Labs Logo" />
+	</div>
 </header>
 
 <style>
@@ -13,11 +16,22 @@
 		position: sticky;
 		top: 0;
 		display: grid;
-		grid-template-columns: 16ch 20ch;
+		grid-template-columns: 16ch 20ch 1fr;
 		gap: 1rem;
 		align-items: center;
 		padding: 1rem 2rem;
 		border-bottom: var(--border-width) solid;
 		background-color: white;
+	}
+
+	.logo-container {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	img {
+		height: 3rem;
+		width: auto;
 	}
 </style>
