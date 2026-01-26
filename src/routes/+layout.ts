14d54f16bda9333d19cbe23b79bb7816ebf5186f
Matt Derocher <matt@derocher.me>
2026-01-26 18:52:05 -0500

    set up tauri

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 // Disable SSR for the entire app (needed for localStorage to work properly)
 export const ssr = false
 export const prerender = false
+export const trailingSlash = 'always'
 // csr defaults to true, which is what we want for client-side rendering
